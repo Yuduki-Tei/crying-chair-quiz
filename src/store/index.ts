ecee93f8c8077415eea6d40f8168ad515519ff24
chengkunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2024-09-12 04:28:31 +0000

    store index update

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,4 @@ export { useOnlineQuestionStore } from "./OnlineQuestionStore";
 export { useUserStore } from "./UserStore";
 export { useResultStore } from "./ResultStore";
 export { useButtonStatusStore } from "./ButtonStatusStore";
+export { useLocalQuestionStore } from "./LocalQuestionStore";
