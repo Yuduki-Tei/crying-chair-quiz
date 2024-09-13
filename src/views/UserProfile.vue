6de9984cdb89a2ad6a60391cdef94e6bb55dc875
chengkunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2024-09-13 08:58:51 +0000

    add radarchart to user profile the tooltip is currently broken

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -8,43 +8,88 @@
   >
   <div class = "row justify-content-center text-center">
     <h3 class="m-3 text-center">使用者資料</h3>
+    <label for="bar">平均正解率 : {{ correctRating }} %</label>
+    <CountdownBar id="bar" :barLength="correctRating" style="height: 8px"/>
     <p>信箱 : {{ mail }} </p>
-    <p>正解率 : {{ correctRating }} %</p>
+  </div>
+  <div
+    class="container d-flex justify-content-center align-items-center"
+    style="max-width: 300px"
+  >
+    <RadarChart v-if="!loading" :correctRates= "catCorrects"/>
   </div>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import { useUserStore } from "../store";
-import { fromBase64, sumBits } from "../composables";
+import { useUserStore, useOnlineQuestionStore } from "../store";
+import { fromBase64, sumBits, getBit } from "../composables";
+import CountdownBar from "../components/CountdownBar.vue";
 import DropDown from "../components/DropDown.vue";
 import Loading from "../components/Loading.vue";
+import RadarChart from "../components/RadarChart.vue";
 
 export default defineComponent({
   name: "UserProfile",
-  components: { DropDown, Loading },
+  components: { DropDown, Loading, RadarChart, CountdownBar },
   setup() {
     const loading = ref(true);
+    const catCorrects =  ref<number[]>([]);
     const user = useUserStore();
     const data = user.dataList;
     const mail = data.user_mail;
+    const online = useOnlineQuestionStore();
+    const allCats = [
+          '文學',
+          '理科',
+          '地理',
+          '社會',
+          '生活',
+          '歷史',
+          '語言',
+          '運動',
+          '文化',
+          '娛樂'
+        ];
 
     let cor_bit = fromBase64(data.correct_history);
     let ans_bit = fromBase64(data.answer_history);
 
     const correctRating = Math.floor((sumBits(cor_bit) / sumBits(ans_bit) || 0) * 10000) / 100;
 
+    const calculateCatCorrect = async (cat: string) => {
+      let qids = new Set(await online.fetchCategoryQids(cat));
+
+      let ans_cnt = 0;
+      let cor_cnt = 0;
+
+      qids.forEach((qid) => {
+        if (qid >= 0 && qid < ans_bit.length * 8 && getBit(ans_bit, qid) === 1) {
+          if (getBit(cor_bit, qid) === 1) {
+            cor_cnt++;
+          }
+          ans_cnt++;
+        }
+      });
+
+      catCorrects.value.push(Math.floor((cor_cnt / ans_cnt || 0) * 10000) / 100);
+    }
+
     onMounted(async() => {
       if (!data.correct_history || !data.answer_history) {
         await user.checkUserAccount();
-      } 
+      };
+      for (const cat of allCats) {
+        await calculateCatCorrect(cat);
+      };
       loading.value = false;
     })
     return {
       mail,
       loading,
       correctRating,
+      catCorrects,
     };
   },
 });
