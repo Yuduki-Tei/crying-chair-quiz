35b1428576a03d581854a8b08cb9d5d434928b39
CKC <100252466+Yuduki-Tei@users.noreply.github.com>
2024-10-03 05:13:30 +0000

    add good button and bad button, sightly change logic

--- src/composables/check.ts
+++ src/composables/check.ts
@@ -4,20 +4,34 @@ import { getBit, setBit, fromBase64, toBase64 } from "./index";
 export function useCheckAnswer(qInd: number, ans: string) {
   const res = useResultStore();
   const qStore = useOnlineQuestionStore();
-  const qid = qStore.getQuestion(qInd).qid;
+  const qData = qStore.getQuestion(qInd);
 
   updateResultStore(qInd, qStore, ans, res);
-  updateLocalBit(qInd, qid, res);
+  updateLocalAnswerBit(qInd, qData.qid, res);
   if (res.getRes(qInd).correct) {
     let base = 60;
-    let x = res.getRes(qInd).interval / qStore.getQuestion(qInd).q_text.length;
+    let x = res.getRes(qInd).interval / qData.q_text.length;
     let bonus = [60, 50, 40, 20, 0][Math.min(4, Math.floor(x / 0.2))];
     res.setRes(qInd, { point: base + bonus });
     res.total += base + bonus;
   }
 }
 
-export function updateResultStore(
+export function buttonGood(qInd: number){
+  updateLocalRatingBit(qInd, 1);
+}
+
+export function buttonBad(qInd: number){
+  updateLocalRatingBit(qInd, 0);
+}
+
+export function getQid(qInd: number){
+  const qStore = useOnlineQuestionStore();
+  const qid = qStore.getQuestion(qInd).qid;
+  return qid
+}
+
+function updateResultStore(
   qInd: number,
   qStore: any,
   ans: string,
@@ -37,7 +51,7 @@ export function updateResultStore(
   res.setRes(qInd, { correct: isMatch });
 }
 
-export function updateLocalBit(qInd: number, qid: number, res: any) {
+function updateLocalAnswerBit(qInd: number, qid: number, res: any) {
   const user = useUserStore();
   const userData = user.dataList;
   let ans_bit = fromBase64(userData.answer_history);
@@ -49,4 +63,18 @@ export function updateLocalBit(qInd: number, qid: number, res: any) {
     cor_bit = setBit(cor_bit, qid, +res.getRes(qInd).correct); //[+bool] becomes a number
     userData.correct_history = toBase64(cor_bit);
   }
+}
+
+function updateLocalRatingBit(qInd: number, rate: number) {
+  const qid = getQid(qInd);
+  const user = useUserStore();
+  const userData = user.dataList;
+  let rate_bit = fromBase64(userData.rate_history);
+  let good_bit = fromBase64(userData.good_history);
+
+  rate_bit = setBit(rate_bit, qid, 1);
+  userData.rate_history = toBase64(rate_bit);
+
+  good_bit = setBit(good_bit, qid, rate);
+  userData.good_history = toBase64(good_bit);
 }
