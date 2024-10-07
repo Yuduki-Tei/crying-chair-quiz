a2da31a825508621d5f57d20dc7cace04cf4de04
chengkunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2024-10-07 02:25:21 +0000

    apply google analytics

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const setupFirebase = getAuth(app);
 export const db = getFirestore(app);
