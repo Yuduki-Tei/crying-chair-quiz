576ef9aa9bb02bc93febef6702dcfc134eb86a17
chengkunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2024-10-11 07:16:04 +0000

    text chagne

--- src/components/HelloBanner.vue
+++ src/components/HelloBanner.vue
@@ -3,6 +3,10 @@
     <p class="mb-1">
       這是一個開發中的Web App，有任何問題都可以從意見表單反映。
     </p>
+    <p class="mt-2 mb-0">2024/10/11</p>
+    <li>修正了資料庫結構。</li>
+    <li>現在評價可以修改與收回。</li>
+    <li>現在使用者的正解紀錄會是你最後一次答題的紀錄，而非第一次。</li>
     <p class="mt-2 mb-0">2024/10/10</p>
     <li>修正了在不同裝置遊玩時資料庫評價紀錄會被錯誤覆蓋的問題。</li>
     <p class="mt-2 mb-0">2024/10/08</p>
@@ -22,7 +26,7 @@ export default defineComponent({
   name: "HelloBanner",
   components: { Modal },
   setup() {
-    const helloID = "20241008";
+    const helloID = "20241011";
     const showModal = ref<boolean>(true);
 
     if (
