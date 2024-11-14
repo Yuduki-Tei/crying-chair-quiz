7d768a1c9e9e7827f49a7f9d2d8208fa740b3fbc
chengkunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2024-11-14 07:19:28 +0000

    delete cat store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 export { useQuestionStore } from "./QuestionStore";
-export { useCatStore } from "./CatStore"
 export { useUserStore } from "./UserStore";
 export { useResultStore } from "./ResultStore";
 export { useButtonStatusStore } from "./ButtonStatusStore";
