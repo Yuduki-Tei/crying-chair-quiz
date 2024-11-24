f7331caf315200d2a4bc29268904d7df5ce2128f
CKC <chengkunchih1998@gmail.com>
2024-11-24 18:53:33 +0900

    text change

--- src/components/HelloBanner.vue
+++ src/components/HelloBanner.vue
@@ -3,13 +3,8 @@
     <p class="mb-1">
       這是一個開發中的Web App，有任何問題都可以從意見表單反映。
     </p>
-    <p class="mt-2 mb-0">2024/11/17</p>
-    <li>本週因為睡眠不足頭很痛，題目停更一週。</li>
-    <li>發現並修復了無法正確與database同步的問題，但過去三周的資料死掉了:(</li>
-    <p class="mt-2 mb-0">2024/11/10</p>
-    <li>更新了每週問題，本週也是關聯題組。</li>
-    <li>稍微修改了部分UI。</li>
-    <li>修改了CSS的引入方式，現在從0開始載入大概會快一點點。</li>
+    <p class="mt-2 mb-0">2024/11/24</p>
+    <li>更新每周十題，是用我在看某部作品的時候看到的keyword出的。</li>
     <p class="mt-2 me-5 text-end">by dev</p>
   </Modal>
 </template>
@@ -21,7 +16,7 @@ export default defineComponent({
   name: "HelloBanner",
   components: { Modal },
   setup() {
-    const helloID = "20241117";
+    const helloID = "20241124";
     const showModal = ref<boolean>(true);
 
     if (
