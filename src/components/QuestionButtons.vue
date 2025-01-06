11813e8f45542a251c679a6a7c535837e5adbbd8
CKC <100252466+Yuduki-Tei@users.noreply.github.com>
2025-01-06 02:53:16 +0000

    eazy impliment for battle button

--- src/components/QuestionButtons.vue
+++ src/components/QuestionButtons.vue
@@ -138,11 +138,13 @@ import {
   onBeforeUnmount,
 } from "vue";
 import { useQuestionStateStore, useUserStore } from "../store";
+import { useSocket } from "../composables";
 
 export default defineComponent({
   name: "QuestionButtons",
   props: {
     curInd: { type: Number, default: 0 },
+    curAns:{ type: String, default: ''},
     onPause: {
       type: Function as PropType<() => void>,
       default: null,
@@ -177,57 +179,59 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const questionState = useQuestionStateStore();
+    const qState = useQuestionStateStore();
     const user = useUserStore();
+    const {emitEvent, socketConnected } = useSocket('https://hfffcf-5000.csb.app/');
 
     const userRating = computed(() =>
       user.getUserRate(Math.min(Math.max(props.curInd, 0), 9))
     );
-    const answerOK = computed(() => questionState.answerOK);
-    const nextOK = computed(() => questionState.nextOK);
-    const stopOK = computed(() => questionState.stopOK);
-    const startOK = computed(() => questionState.startOK);
-    const rateOK = computed(() => questionState.rateOK);
-    const hintOK = computed(
-      () => questionState.hintOK && !questionState.isWeekly
-    );
-    const plusTimeOK = computed(
-      () => questionState.plusTimeOK && !questionState.isWeekly
-    );
-    const plusTextOK = computed(
-      () => questionState.plusTextOK && !questionState.isWeekly
-    );
-    const funcsOK = computed(
-      () => !questionState.isWeekly && questionState.answerOK
-    );
+    const answerOK = computed(() => qState.answerOK);
+    const nextOK = computed(() => qState.nextOK);
+    const stopOK = computed(() => qState.stopOK);
+    const startOK = computed(() => qState.startOK);
+    const rateOK = computed(() => qState.rateOK);
+    const hintOK = computed(() => qState.hintOK && !qState.isWeekly);
+    const plusTimeOK = computed(() => qState.plusTimeOK && !qState.isWeekly);
+    const plusTextOK = computed(() => qState.plusTextOK && !qState.isWeekly);
+    const funcsOK = computed(() => !qState.isWeekly && qState.answerOK);
 
     const pauseQuestion = () => {
-      questionState.pauseQuestion();
+      qState.pauseQuestion();
       props.onPause && props.onPause();
+      if (socketConnected){
+        emitEvent('sync_pause', qState.curPos);
+      }
     };
 
     const displayNextQuestion = () => {
-      questionState.displayQuestion();
+      qState.displayQuestion();
       props.onNext && props.onNext();
+      if (socketConnected){
+        emitEvent('sync_ready');
+      }
     };
 
     const checkAnswer = () => {
-      questionState.submitAnswer();
+      qState.submitAnswer();
       props.onAnswer && props.onAnswer();
+      if (socketConnected){
+        emitEvent('sync_answer', props.curAns);
+      }
     };
 
     const getOneWord = () => {
-      questionState.hintOK = false;
+      qState.hintOK = false;
       props.onHint && props.onHint();
     };
 
     const plusTime = () => {
-      questionState.plusTimeOK = false;
+      qState.plusTimeOK = false;
       props.onPlusTime && props.onPlusTime();
     };
 
     const plusText = () => {
-      questionState.plusTextOK = false;
+      qState.plusTextOK = false;
       props.onPlusText && props.onPlusText();
     };
 
