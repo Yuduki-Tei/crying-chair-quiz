f1c22499ac5428f94422b59b94c3b4018537705b
CKC <100252466+Yuduki-Tei@users.noreply.github.com>
2025-01-06 03:57:16 +0000

    arg 'isWeekly' moved to QuestionStateStore

--- src/store/ResultStore.ts
+++ src/store/ResultStore.ts
@@ -9,14 +9,10 @@ interface resData {
 
 export const useResultStore = defineStore("Result", {
   state: () => ({
-    isWeekly: false, //status to check if function buttons are available
     total: 0,
     dataList: [] as resData[],
   }),
   actions: {
-    setType(type: string) {
-      this.isWeekly = type === "weekly";
-    },
     initDataList() {
       var data = Array.from({ length: 10 }, () => ({
         answer: "",
@@ -30,7 +26,6 @@ export const useResultStore = defineStore("Result", {
     clearDataList() {
       this.total = 0;
       this.dataList = [];
-      this.isWeekly = false;
     },
     getRes(index: number) {
       if (index >= 0 && index < this.dataList.length) {
