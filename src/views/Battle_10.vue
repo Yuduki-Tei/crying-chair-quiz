5304809b21787d811e6d5361c7795a47a6c54418
CKC <100252466+Yuduki-Tei@users.noreply.github.com>
2025-01-08 07:06:35 +0000

    use this to connect socket

--- src/views/Battle_10.vue
+++ src/views/Battle_10.vue
@@ -1,10 +1,12 @@
 <template>
+<div v-if="connected">
   <Quiz pageTitle="對戰十題" qType="battle"/>
   <MatchingRoom />
+</div> 
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onUnmounted, computed } from 'vue';
 import Quiz from "../components/Quiz.vue";
 import MatchingRoom from "../components/MatchingRoom.vue";
 import { useQuestionStateStore, useSocketStore } from '../store';
@@ -12,6 +14,20 @@ import { useQuestionStateStore, useSocketStore } from '../store';
 export default defineComponent({
   name: 'Battle_10',
   components: { MatchingRoom, Quiz },
+  setup(){
+    const socket = useSocketStore();
+    socket.connect(import.meta.env.VITE_TMP_SOCKET_URL);
+    const connected = computed(() => socket.socketConnected);
+    onUnmounted(() => {
+      if (socket.value){
+        socket.removeAllListeners();
+        socket.disconnect();
+      }
+    });
+    return {
+      connected,
+    }
+  }
 });
 </script>
 
