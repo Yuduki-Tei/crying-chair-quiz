0e36f97a2458ae041b90bfaad722fffbbe210b47
CKC <100252466+Yuduki-Tei@users.noreply.github.com>
2025-01-09 07:11:19 +0000

    add some listener

--- src/views/Battle_10.vue
+++ src/views/Battle_10.vue
@@ -1,15 +1,17 @@
 <template>
 <div v-if="connected">
-  <Quiz pageTitle="對戰十題" qType="battle"/>
   <MatchingRoom />
+</div>
+<div v-if="allReady">
+  <Quiz pageTitle="對戰十題" :qType="mode"/>
 </div> 
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref, onUnmounted, computed } from 'vue';
 import Quiz from "../components/Quiz.vue";
 import MatchingRoom from "../components/MatchingRoom.vue";
-import { useQuestionStateStore, useSocketStore } from '../store';
+import { useSocketStore } from '../store';
 
 export default defineComponent({
   name: 'Battle_10',
@@ -18,14 +20,35 @@ export default defineComponent({
     const socket = useSocketStore();
     socket.connect(import.meta.env.VITE_TMP_SOCKET_URL);
     const connected = computed(() => socket.socketConnected);
+    const allReady = ref<boolean>(false);
+    const mode = ref<string>('battle');
+
+    const updateModeText = (roomId: string) => {
+      mode.value += roomId;
+    }
+
+    socket.onEvent('room_created', (data: any) => {
+      updateModeText(data.room_id);
+    })
+
+    socket.onEvent('room_left', () => {
+      allReady.value = false;
+    })
+
+    socket.onEvent('all_ready', () => {
+      allReady.value = true;
+    })
+
+
     onUnmounted(() => {
-      if (socket.value){
-        socket.removeAllListeners();
+      if (socket){
         socket.disconnect();
       }
     });
     return {
+      allReady,
       connected,
+      mode,
     }
   }
 });
